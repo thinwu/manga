58f39cf841c245f4628642e0c71b4c04386c2dfd
Wu, Bin (Daniel) <binwu@ea.com>
2019-04-30 18:54:31 +0800

    submit for feature release

---
+++ MangaFetch/MangaFetch/MangaSpiders.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MangaFetch
+{
+    abstract class Utilities
+    {
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hWnd);
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            w.WriteLine("  :");
+            w.WriteLine($"  :{logMessage}");
+            w.WriteLine("-------------------------------");
+        }
+        public static void CloseIE(string titleLike)
+        {
+            Process[] processes = Process.GetProcessesByName("iexplore");
+
+            foreach (Process proc in processes)
+            {
+                string title = proc.MainWindowTitle;
+                if (titleLike.Equals(String.Empty))
+                {
+                    proc.CloseMainWindow();
+                }
+                else if (title.Contains(titleLike))
+                {
+                    proc.CloseMainWindow();
+                }
+                
+            }
+        }
+        public static void MainWindowPressEnter()
+        {
+            Process[] processes = Process.GetProcessesByName("iexplore");
+
+            foreach (Process proc in processes)
+            {
+                SetForegroundWindow(proc.MainWindowHandle);
+                SendKeys.SendWait("{ENTER}");
+
+
+            }
+        }
+    }
+    abstract class MangaSpiders
+    {
+        static int generalSleep = 200;
+        public static void XXMH(string URL, string subFolder, ref float vol)
+        {
+            var IE = new SHDocVw.InternetExplorer();
+            IE.Visible = true;
+            IE.Navigate2(URL);
+            string pwd = Directory.GetCurrentDirectory();
+
+            string logFile = Path.Combine(pwd, "MangaSpider.log");
+            string[] tabServer = { "tab_srv1", "tab_srv2", "tab_srv3", "tab_srv4", "tab_srv5" };
+            
+            while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
+            {
+                Thread.Sleep(generalSleep);
+            }
+            string mangaFolder = Path.Combine(pwd, subFolder);
+            int serverIndex = 0;
+            IE.Document.getElementById(tabServer[serverIndex]).click();
+            while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
+            {
+                Thread.Sleep(generalSleep);
+            }
+            int totalPages = IE.Document.getElementsByClassName("selectTT")[0].getElementsByTagName("option").Length;
+            string currentTitle = IE.Document.IHTMLDocument2_nameProp;
+            string src = "";
+            WebClient webClient = new WebClient();
+            try
+            {
+                float newVol = float.Parse(Regex.Match(currentTitle, @"\d+").Value);
+                vol = vol == newVol ? (vol += 0.1f) : (vol = newVol);
+            }
+            catch
+            {
+                vol += 0.1f;
+            }
+            string result = (vol / 1000).ToString().Replace(".", "");
+            if (result.Length == 4)
+            {
+                result = result + "0";
+            }
+            if (result.Length == 3)
+            {
+                result = result + "00";
+            }
+            if (result.Length == 2)
+            {
+                result = result + "000";
+            }
+            bool switchServer = false;
+            string folder = "";
+            //true -and 
+            string fileName = "0";
+            folder = String.Format(@"{0}\{1}", mangaFolder, currentTitle).Replace('?', '!');
+            System.IO.Directory.CreateDirectory(folder);
+            bool skip = false;
+            for (int index = 1; index<= totalPages; index++)
+            {
+                string page = ((index / 100.00).ToString()).Replace(".", "");
+                if (page.Length == 2)
+                {
+                    page = page + "0";
+                }
+                serverIndex = 0;
+                fileName = page;
+                fileName = String.Format(folder + @"\{0}.jpg", result.ToString() + "_" + page.ToString());
+                webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko");
+                webClient.Headers.Add("X-UA-Compatible", "IE=11");
+                while (true)
+                {
+                    while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
+                    {
+                        Thread.Sleep(generalSleep);
+                    }
+                    var element = IE.Document.getElementById("dracga");
+                    src = element.src;
+                    string logLine = src + " as " + fileName;
+                    while (src.Contains("gif") || switchServer)
+                    {
+                        serverIndex++;
+                        if (serverIndex < tabServer.Length)
+                        {
+                            IE.Document.getElementById(tabServer[serverIndex]).click();
+                            while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
+                            {
+                                Thread.Sleep(generalSleep);
+                            }
+                            element = IE.Document.getElementById("dracga");
+                            src = element.src;
+                            switchServer = false;
+                        }
+                        else
+                        {
+                            skip = true;
+                            break;
+                        }
+                    }
+                    try
+                    {
+                        if (!skip)
+                        {
+                            webClient.DownloadFile(src, fileName);
+                            switchServer = false;
+                            Console.Out.WriteLine(logLine);
+                            using (StreamWriter w = File.AppendText(logFile))
+                            {
+                                Utilities.Log(logLine, w);
+                            }
+                        }
+                        else
+                        {
+                            logLine = "skipped: " + src + " as " + fileName;
+                            Console.Out.WriteLine("skipped: " + src + " as " + fileName);
+                            using (StreamWriter w = File.AppendText(logFile + ".skppedList"))
+                            {
+                                Utilities.Log(logLine, w);
+                            }
+                        }
+                        if (index < totalPages)
+                        {
+                            element.click();
+                        }
+                        serverIndex = 0;
+                        break;
+                    }
+                    catch
+                    {
+                        switchServer = true;
+                        continue;
+                    }
+                }
+            }
+            Utilities.CloseIE(subFolder);
+        }
+        public static void XXMHV2(string URL, int StartPage = 1)
+        {
+            var IE = new SHDocVw.InternetExplorer();
+            IE.Visible = true;
+            IE.Navigate2(URL);
+
+            string pwd = Directory.GetCurrentDirectory();
+            string logFile = Path.Combine(pwd, "MangaSpider.log");
+            
+
+            while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
+            {
+                Thread.Sleep(generalSleep);
+            }
+            string subFolder = IE.Document.IHTMLDocument2_nameProp.ToString();
+            subFolder = subFolder.Split(' ')[0];
+            var TitleLi = IE.Document.getElementById("coclist1").getElementsByTagName("li");
+            int fatchSize = TitleLi.Length - StartPage + 1;
+            string[] VolURLs = new string[fatchSize];
+            int j = 0;
+            for (int i = fatchSize-1; i >= 0; i--)
+            {
+                VolURLs[j] = TitleLi[i].getElementsByTagName("a")[0].IHTMLAnchorElement_href;
+                j++;
+            }
+            //TitleLi[0].getElementsByTagName("a")[0]
+            //		IHTMLAnchorElement_href
+
+            float vol = 0.0f;
+            Utilities.CloseIE(subFolder);
+            foreach(string url in VolURLs)
+            {
+                XXMH(url, subFolder, ref vol);
+            }
+        }
+        public static void KUKUKKK(dynamic IE, string subFolder)
+        {
+
+        }
+    }
+}
--- MangaFetch/MangaFetch/Program.cs
+++ MangaFetch/MangaFetch/Program.cs
@@ -12,225 +12,32 @@ namespace MangaFetch
 {
     class Program
     {
-        [DllImport("user32.dll")]
-        public static extern int SetForegroundWindow(IntPtr hWnd);
-        public static void Log(string logMessage, TextWriter w)
-        {
-            w.Write("\r\nLog Entry : ");
-            w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
-            w.WriteLine("  :");
-            w.WriteLine($"  :{logMessage}");
-            w.WriteLine("-------------------------------");
-        }
-        public static void MainWindowPressEnter(Process[] processes)
-        {
-            processes = Process.GetProcessesByName("iexplore");
-
-            foreach (Process proc in processes)
-            {
-                SetForegroundWindow(proc.MainWindowHandle);
-                SendKeys.SendWait("{ENTER}");
-
-
-            }
-        }
+        
         static void Main(string[] args)
         {
-            Process[] processes = Process.GetProcessesByName("iexplore");
 
-            foreach (Process proc in processes)
-            {
-                proc.Close();
-            }
-            var IE = new SHDocVw.InternetExplorer();
-            Console.WriteLine("The starter Volumn on www.177mh.net");
-            object URL = Console.ReadLine(); // "https://www.177mh.net/201301/239684.html";//"https://www.177mh.net/201902/409391.html";
-            Console.WriteLine("Folder to place the Volumns, default is 'Manga'");
-            string subFolder = Console.ReadLine();
-            subFolder = subFolder == String.Empty ? "Manga" : subFolder;
-            IE.Visible = true;
-            IE.Navigate2(ref URL);
-            int generalSleep = 200;
-            string pwd = Directory.GetCurrentDirectory();
-            string mangaFolder = Path.Combine(pwd, subFolder);
-            string logFile = Path.Combine(pwd, "MangaSpider.log");
-            string[] tabServer = { "tab_srv1", "tab_srv2", "tab_srv3", "tab_srv4", "tab_srv5"};
             
-            while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
+            Console.WriteLine("The starter Volumn on www.177mh.net or comic.kukukkk.com");
+            string URL = Console.ReadLine(); // "https://www.177mh.net/201301/239684.html";//"https://www.177mh.net/201902/409391.html";
+            Console.WriteLine("Starts From:");
+            string startPage = Console.ReadLine();
+            if (URL.ToString().Contains("www.177mh.net"))
             {
-                Thread.Sleep(generalSleep);
-            }
-            int serverIndex = 0;
-            IE.Document.getElementById(tabServer[serverIndex]).click();
-            while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
-            {
-                Thread.Sleep(generalSleep);
-            }
-            string currentTitle = "";
-            int index = 1;
-            int retry = 5;
-            string src = "";
-            WebClient webClient = new WebClient();
-            string olderURL = "";
-            float vol = 0.0f;
-
-            bool switchServer = false;
-            string folder = "";
-            //true -and 
-            string fileName = "0";
-            while (retry>0)
-            {
-                bool taskResult = false;
-                Thread.Sleep(2);
-                if (switchServer)
+                if (startPage.Equals(String.Empty))
                 {
-                    if (serverIndex >= 5)
-                    {
-                        using (StreamWriter w = File.AppendText(logFile))
-                        {
-                            Log("running out of switching servers", w);
-                            using (StreamWriter imgBroken = File.AppendText(fileName + "." + "broken"))
-                            {
-                                Log("running out of switching servers", w);
-                            }   
-                        }
-                        taskResult = Task.Run(() => { IE.Document.getElementById("dracga").click(); }).Wait(3000);
-                        if (!taskResult)
-                        {
-                            MainWindowPressEnter(processes);
-                        }
-                    }
-                    else
-                    {
-                        IE.Document.getElementById(tabServer[serverIndex]).click();
-                        while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
-                        {
-                            Thread.Sleep(generalSleep);
-                        }
-                        serverIndex++;
-                    }
-                    
+                    MangaSpiders.XXMHV2(URL);
                 }
-                Thread.Sleep(generalSleep);
-                if (!currentTitle.Equals(IE.Document.IHTMLDocument2_nameProp)){
-                    currentTitle = IE.Document.IHTMLDocument2_nameProp;
-                    serverIndex = 0;
-                    try
-                    {
-                        float newVol = float.Parse(Regex.Match(currentTitle, @"\d+").Value);
-                        vol = vol == newVol ? (vol += 0.1f) : (vol = newVol);
-                    }
-                    catch
-                    {
-                        vol += 0.1f;
-                    }
-                    index = 1;
+                else
+                {
+                    MangaSpiders.XXMHV2(URL,int.Parse(startPage));
                 }
                 
-                string result = (vol / 1000).ToString().Replace(".", "");
-                string page = ((index / 100.00).ToString()).Replace(".", "");
-                if (page.Length == 2){
-                    page = page + "0";
-                 }
-                if (result.Length == 4){
-                    result = result + "0";
-                }
-                if (result.Length == 3){
-                    result = result + "00";
-                }
-                if (result.Length == 2){
-                    result = result + "000";
-                }
-                folder = String.Format(@"{0}\{1}", mangaFolder, currentTitle + "_" + result.ToString()).Replace('?', '!');
-                System.IO.Directory.CreateDirectory(folder);
-                var element = IE.Document.getElementById("dracga");
-                fileName = page;
-                fileName = String.Format(folder+@"\{0}.jpg", result.ToString()+"_"+page.ToString());
-                if ( !src.Equals( element.src) || switchServer ){
-                    src = element.src;
-                    bool skip = false;
-                    if (src.Contains("gif"))
-                    {
-                        skip = true;
-                    }
-                    while (true)
-                    {
-                        try
-                        {
-                            
-                            webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko");
-                            webClient.Headers.Add("X-UA-Compatible", "IE=11");
-                            string logLine = src + " as " + fileName;
-                            if (!skip)
-                            {
-                                webClient.DownloadFile(src, fileName);
-                                Console.Out.WriteLine(logLine);
-                                using (StreamWriter w = File.AppendText(logFile))
-                                {
-                                    Log(logLine, w);
-                                }
-                            }
-                            else
-                            {
-                                logLine = "skipped: " + src + " as " + fileName;
-                                Console.Out.WriteLine("skipped: " + src + " as " + fileName);
-                                using (StreamWriter w = File.AppendText(logFile+".skppedList"))
-                                {
-                                    Log(logLine, w);
-                                }
-                            }
-                            
-                            switchServer = false;
-                            serverIndex = 0;
-                            if (olderURL == IE.LocationURL)
-                            {
-                                retry--;
-                            }
-                            else
-                            {
-                                retry = 5;//reset
-                                olderURL = IE.LocationURL;
-                            }
-                            olderURL = IE.LocationURL;
-                            taskResult = Task.Run(() => { element.click(); }).Wait(3000);
-                            while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
-                            {
-                                Thread.Sleep(2);
-                            }
-                            if (!taskResult)
-                            {
-                                while (olderURL == IE.LocationURL && retry > 0)
-                                {
-                                    MainWindowPressEnter(processes);
-                                    while (IE.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
-                                    {
-                                        Thread.Sleep(generalSleep);
-                                    }
-                                    Thread.Sleep(generalSleep);
-                                    retry--;
-                                    index = 1;
+            }
+            else if (URL.ToString().Contains("comic.kukukkk.com"))
+            {
 
-                                }
-                            }
-                            else
-                            {
-                                if (!skip)
-                                {
-                                    index = index + 1;
-                                }  
-                            }
-                            break;
-
-                        }
-                        catch
-                        {
-                            switchServer = true;
-                            break;
-                        }
-                    }
-                    
-                }
             }
         }
+        
     }
 }
